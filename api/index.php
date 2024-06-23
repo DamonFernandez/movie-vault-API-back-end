d009b27c63e5feb04cc507adcd691e8438cc0716
Damon Fernandez <damonfernandez04@gmail.com>
2024-06-22 20:25:46 -0400

    add /completedwatchlist/entries endpoint

--- api/index.php
+++ api/index.php
@@ -1,35 +1,5 @@
 <?php require "../includes/library.php";
 
-function getUserAPIKey($pdo){
-    if (!isset($_SERVER['HTTP_X_API_KEY']) || empty($_SERVER['HTTP_X_API_KEY'])) {
-      header('HTTP/1.1 400 Bad Request');
-      echo json_encode(['error' => 'You must provide an API key']);
-      exit();
-  }
-  $userApiKey = $_SERVER['HTTP_X_API_KEY'];
-  
-    $stmt = $pdo->prepare("SELECT 1 FROM `users` WHERE `apikey` = ?");
-    $stmt->execute([$userApiKey]);
-    $isValidApiKey = $stmt->fetchColumn();
-  
-    if ($isValidApiKey === false) {
-      header('HTTP/1.1 400 Bad Request');
-      echo json_encode(['error' => 'The provided API key is invalid']);
-      exit();
-  }
-  return $stmt["api_key"];
-  }
-
-  function queryDB($pdo, $query, $arrayOfValuesToPass){
-    $stmt = $pdo -> prepare($query);
-    return $stmt -> execute();
-  }
-
-function getUserID($pdo, $userApiKey){
-    $query = "SELECT user_id FROM users WHERE api_key = ?";
-    queryDB($pdo, $query, [$userApiKey]);
-}
-
 function getEndPoint(){
     $uri = $_SERVER["REQUEST_URI"];
     $uri = parse_url($uri);
@@ -37,13 +7,49 @@ function getEndPoint(){
     $endpoint = str_replace(__BASE__, "", $uri["path"]);
     return $endpoint;
 }
+function getUserAPIKey($pdo){
+    // Check that its set
+    if (!isset($_SERVER['HTTP_X_API_KEY']) || empty($_SERVER['HTTP_X_API_KEY'])) {
+      header('HTTP/1.1 400 Bad Request');
+      echo json_encode(['error' => 'You must provide an API key']);
+      exit();
+  }
+  $userApiKey = $_SERVER['HTTP_X_API_KEY'];
+  
 
+  // Check if its valid
+    $stmt = $pdo->prepare("SELECT 1 FROM `users` WHERE `apikey` = ?");
+    $stmt->execute([$userApiKey]);
+    $isValidApiKey = $stmt->fetchColumn();
+    if ($isValidApiKey === false) {
+      header('HTTP/1.1 400 Bad Request');
+      echo json_encode(['error' => 'The provided API key is invalid']);
+      exit();
+  }
+  return $userApiKey;
+  }
 
+  function queryDB($pdo, $query, $arrayOfValuesToPass){
+    $stmt = $pdo -> prepare($query);
+     $stmt -> execute($arrayOfValuesToPass);
+     return $stmt;
+  }
 
+function getUserID($pdo, $userApiKey){
+    $query = "SELECT user_id FROM users WHERE api_key = ?";
+    $queryResultSetObject = queryDB($pdo, $query, [$userApiKey]);
+    $result = $queryResultSetObject -> fetch();
+    return $result["user_id"];
+}
+
+  // GLOBAL CODE
   $endpoint = getEndPoint();
   $requestMethod = $_SERVER['REQUEST_METHOD'];
   $pdo = connectdb();
 
+
+  // All endpoints that dont involve the movies table require an API key
+  // So get it if the endpoint does not contain "movies" in its name
   if(!str_contains($endpoint, "movies")){
     $userApiKey = getUserAPIKey($pdo);
     $user_id = getUserID($pdo, $userApiKey);
@@ -51,7 +57,13 @@ function getEndPoint(){
 
 if($requestMethod == "GET" && $endpoint == "/completedwatchlist/entries"){
     $query = "SELECT * FROM completedWatchList WHERE userID = ?";
-    $queryResultSet = queryDB($pdo, $query, [$user_id]);
+    $queryResultSetObject = queryDB($pdo, $query, [$user_id]);
+    $completedWatchList = $queryResultSetObject -> fetchAll();
+    $completedWatchList = json_encode($completedWatchList);
+
+    header("Content-Type: application/json; charset=UTF-8"); 
+    header("HTTP/1.1 200 OK");
+    echo $completedWatchList;
 }
 
   
