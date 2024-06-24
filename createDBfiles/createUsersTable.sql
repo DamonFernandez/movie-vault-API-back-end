a883f253d4fb8ca4db4884b196bbba3bee6d966b
Blitzcranq <vrajchauhan2110@gmail.com>
2024-06-24 02:50:42 -0400

    resetting of the tables

--- createDBfiles/createUsersTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users(
-    user_id BIGINT AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE, 
-    password CHAR(60) NOT NULL,
-    api_key CHAR(64) NOT NULL,
-    api_date DATE NOT NULL,
-    PRIMARY KEY (user_id)
-);
