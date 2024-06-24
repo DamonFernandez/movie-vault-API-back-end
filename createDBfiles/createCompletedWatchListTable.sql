b31b568e813059396174989509c8e0ec3cc49225
Blitzcranq <vrajchauhan2110@gmail.com>
2024-06-24 03:02:09 -0400

    reset database tabels

---
+++ createDBfiles/createCompletedWatchListTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `completedWatchList` (
+    `completedWatchListID` int(11) NOT NULL AUTO_INCREMENT,
+    `userID` int(11) NOT NULL,
+    `movieID` int(11) NOT NULL,
+    `rating` int(11) NOT NULL,
+    `notes` text NOT NULL,
+    `dateStarted` date NOT NULL,
+    `dateLastWatched` date NOT NULL,
+    `numOfTimesWatched` int(11) NOT NULL,
+    PRIMARY KEY (`completedWatchListID`),
+    FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+    FOREIGN KEY (`movieID`) REFERENCES `movies` (`movieID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
+++ createDBfiles/createToWatchList.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE IF NOT EXISTS `toWatchList` (
+    `toWatchListID` int(11) NOT NULL AUTO_INCREMENT,
+    `movieID` int(11) NOT NULL,
+    `userID` int(11) NOT NULL,
+    `priority` int(11) NOT NULL,
+    `notes` text NOT NULL,
+    PRIMARY KEY (`toWatchListID`),
+    FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
+    FOREIGN KEY (`movieID`) REFERENCES `movies` (`movieID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
