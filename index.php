86f9a68186624ed6933166b52e3fb18ab8c47339
Blitzcranq <vrajchauhan2110@gmail.com>
2024-06-24 03:36:53 -0400

    link stylesheets to index and view-account

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ checkToRedirectToLoginPage() ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>API Documentation Page</title>
+    <link rel="stylesheet" href="./styles/main.css">
 </head>
 
 <body>
--- view-account.php
+++ view-account.php
@@ -56,7 +56,8 @@ checkForLogOut();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Account Details</title>
+    <link rel="stylesheet" href="./styles/main.css">
 </head>
 
 <body>
